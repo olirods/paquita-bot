f35451adc02c2601387ab4a260aea8f9ad295981
olirods <sergio.olivares@doofinder.com>
2023-07-02 21:47:33 +0200

    very draft version of shopping list

--- config/clock.rb
+++ config/clock.rb
@@ -18,16 +18,16 @@ module Clockwork
 
       elements = notion_client.database_query(database_id: '629199223c6949f4afde6ecf4d871aa9', filter: filter)
 
-      list = ''
+      list = []
       elements.results.map do |item|
-        list << ', ' + item.properties.Name.title[0].plain_text
+        list << item.properties.Name.title[0].plain_text
       end
 
-      telegram_client.send_message(list)
+      telegram_client.send_message("Hola chiques, en vuestra lista de la compra tenéis apuntado: #{list.join(', ')}")
     end
   end
 
-  every(1.day, 'frequent.job') do
+  every(1.day, 'Send shopping list update', at: '18:00', tz: "Europe/Belgrade") do
     send_shopping_list_update
   end
 end
