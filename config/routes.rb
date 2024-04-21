81fb26ffe85a9c8052485ffde2d545600d92c757
Sergio Olivares <sergio.olivares.rodriguez@aon.es>
2024-04-21 17:21:01 +0200

    feat: redirect home to linkedin

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: redirect("https://linkedin.com/in/olirods")
+
 end
